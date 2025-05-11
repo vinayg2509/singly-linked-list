1f4cce1665eff165209122ccf0e7b9ceff52d960
vinayg2509 <vinayg2509@gmail.com>
2025-05-11 20:44:40 +0530

    [Vinay]Added:Added simple linked list with nodes 56, 30, and 70

--- src/main/java/com/bridgelabz/linkedlist/SingleLinkedList.java
+++ src/main/java/com/bridgelabz/linkedlist/SingleLinkedList.java
@@ -1,9 +1,19 @@
 package com.bridgelabz.linkedlist;
 
+import java.util.LinkedList;
+
 public class SingleLinkedList<E>
 {
     public static void main(String[] args)
     {
+        LinkedList<Integer>linkedList= new LinkedList<>();
+        linkedList.add(56);
+        linkedList.add(30);
+        linkedList.add(70);
 
+        for(Integer list:linkedList)
+        {
+            System.out.println(list);
+        }
     }
 }
